22882017eb727746f2d954e442a504312c8cff46
iansyahr <37446681+iansyahr@users.noreply.github.com>
2020-07-03 19:56:44 +0700

    update

---
+++ component---src-pages-tags-jsx-f4f5fd433915dc121104.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{PcuZ:function(t,e,a){"use strict";a.r(e);a("q1tI");var n=a("tCqT"),c=a("TiLl"),u=a("qKvR");e.default=function(t){var e=t.pageContext.tags;return Object(u.d)(n.d,null,Object(u.d)(c.b,{title:"Tags Page"},"Gatsby Tutorial Starter"),Object(u.d)(n.a,null,Object(u.d)(c.e,{list:e})))}}}]);
+//# sourceMappingURL=component---src-pages-tags-jsx-f4f5fd433915dc121104.js.map
